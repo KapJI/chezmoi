38b949aebd38d0fef33e029a2cafb3b1937ab4de
Ruslan Sayfutdinov <ruslan@sayfutdinov.com>
2024-10-11 19:25:44 +0100

    feat: Add darwinVersion to template data

--- internal/cmd/util_unix.go
+++ internal/cmd/util_unix.go
@@ -4,6 +4,9 @@ package cmd
 
 import (
 	"io/fs"
+	"os/exec"
+	"runtime"
+	"strings"
 	"syscall"
 
 	"github.com/twpayne/chezmoi/v2/internal/chezmoi"
@@ -15,6 +18,33 @@ var defaultInterpreters = make(map[string]chezmoi.Interpreter)
 
 func fileInfoUID(info fs.FileInfo) int {
 	return int(info.Sys().(*syscall.Stat_t).Uid) //nolint:forcetypeassert
+}
+
+func darwinVersion() (map[string]any, error) {
+	if runtime.GOOS != "darwin" {
+		return nil, nil
+	}
+	darwinVersion := make(map[string]any)
+	cmdOutput, err := exec.Command("sw_vers").Output()
+	if err != nil {
+		return nil, err
+	}
+	output := string(cmdOutput)
+	lines := strings.Split(output, "\n")
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		parts := strings.Split(line, ":")
+		if len(parts) != 2 {
+			continue
+		}
+		key := strings.TrimSpace(parts[0])
+		key = strings.ToLower(key[:1]) + key[1:]
+		value := strings.TrimSpace(parts[1])
+		darwinVersion[key] = value
+	}
+	return darwinVersion, nil
 }
 
 func windowsVersion() (map[string]any, error) {
